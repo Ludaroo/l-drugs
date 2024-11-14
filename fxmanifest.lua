6738e8d3d45ebfd4600e9bb4a4da6191bda5437b
Emma <151248139+CuzImStupi4@users.noreply.github.com>
2024-11-14 17:43:48 +0100

    fixed fxmanifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,32 +6,32 @@ author("Ludaro & Emma")
 version("1.0.0")
 
 client_scripts({
-	'client/init.lua'
-	"client/*.lua",
+    "client/init.lua",
+    "client/*.lua"
 })
 
 server_scripts({
-	"@oxmysql/lib/MySQL.lua",
-	"server/*.lua",
+    "@oxmysql/lib/MySQL.lua",
+    "server/*.lua"
 })
 
 shared_scripts({
-	"shared/*.lua",
-	"@ox_lib/init.lua",
-	"config/*.lua"
+    "shared/*.lua",
+    "@ox_lib/init.lua",
+    "config/*.lua"
 })
 
 ui_page({
-	"html/dist/index.html",
+    "html/dist/index.html",
 })
 
-dependencies {(
-	'ox_lib',
-	'oxmysql',
-	'es_extended'
-)}
+dependencies {
+    "ox_lib",
+    "oxmysql",
+    "es_extended"
+}
 
 files({
-	'locales/*.json'
-	"html/dist/**",
+    "locales/*.json",
+    "html/dist/**"
 })
