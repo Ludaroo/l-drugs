0c0db77b239efee28f395164141c1ef85df0dc46
Ludaro1024 <waschmaschvanlu@gmail.com>
2024-11-15 14:32:30 +0100

    cleaned up old code

--- client/init.lua
+++ client/init.lua
@@ -2,24 +2,18 @@
 
 
 Effects = {}
-if (GetResourceState("es_extended") == "started") then
-    -- Debug(3, "es_extended resource is started.")
-
 
 if (GetResourceState("es_extended") == "started") then
     if (exports["es_extended"] and exports["es_extended"].getSharedObject) then
         ESX = exports["es_extended"]:getSharedObject()
-
     else
         TriggerEvent("esx:getSharedObject", function(obj)
             ESX = obj
-        -- Debug(2, "ESX shared object retrieved via exports.")
     else
         TriggerEvent("esx:getSharedObject", function(obj)
             ESX = obj
-            -- Debug(2, "ESX shared object retrieved via event.")
-
         end)
     end
+end
 end
 lib.locale()
