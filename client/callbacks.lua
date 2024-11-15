f6d1ed890b09289c969ec45f9e99799b14d75f26
Ludaro1024 <waschmaschvanlu@gmail.com>
2024-11-15 10:42:47 +0100

    added callbacks client side and added toggle ui function

--- client/callbacks.lua
+++ client/callbacks.lua
@@ -1,3 +1,31 @@
-function callbacks_getDrugs()
-return lib.callback.await('l-drugs:getDrugs', false)
+function callback_getDrugs()
+    local result = lib.callback.await('l-drugs:getDrugs', false)
+    if result == nil and result[2] ~= nil then
+        error("Failed to get drugs " .. result[2])
+    end
+    return result
+end
+
+function callback_AddDrug(name, description, effect_type, default_effect_params, max_duration, default_value)
+    local result = lib.callback.await('l-drugs:addDrug', false, name, description, effect_type, default_effect_params, max_duration, default_value)
+    if result == nil and result[2] ~= nil then
+        error("Failed to add drug: " .. result[2])
+    end
+    return result
+end
+
+function calback_removeDrug(name)
+    local result = lib.callback.await('l-drugs:removeDrug', false, name)
+    if result == nil and result[2] ~= nil then
+        error("Failed to remove drug: " .. result[2])
+    end
+    return result
+end
+
+function callback_getPlayerTolerance(drugId)
+    local result = lib.callback.await('l-drugs:getPlayerTolerance', false, drugId)
+    if result == nil and result[2] ~= nil then
+        error("Failed to get player tolerance: " .. result[2])
+    end
+    return result
 end
--- client/ui/ui.lua
+++ client/ui/ui.lua
@@ -1,26 +1,35 @@
-uishown = false
-RegisterCommand("showIndex", function(source, args, rawcommand)
+-- uishown = false
+-- RegisterCommand("showIndex", function(source, args, rawcommand)
+--     SetNuiFocus(not uishown, uishown)
+--     SetNuiFocusKeepInput(not uishown)
+--     SendNUIMessage({
+--         type = "index",
+--     })
+-- end)
+
+-- RegisterCommand("SwitchSites", function(source, args, rawcommand)
+--     SendNUIMessage({
+--         switch = true,
+--     })
+-- end)
+
+function ui_ToggleUI()
     SetNuiFocus(not uishown, uishown)
     SetNuiFocusKeepInput(not uishown)
+    uishown = not uishown
     SendNUIMessage({
         type = "index",
     })
-end)
-
-RegisterCommand("SwitchSites", function(source, args, rawcommand)
-    SendNUIMessage({
-        switch = true,
-    })
-end)
+end
 
 RegisterNUICallback('ToggleUI', function(data, cb)
     -- Toggle the UI visibility
-
+    ui_ToggleUI()
     
     -- Acknowledge the callback without returning any data
     cb(nil)
 end)
 
 RegisterNUICallback('getDrugs', function(data, cb)
-    cb(json.encode(Drugs))
+    cb(json.encode(callback_getDrugs()))
 end)
